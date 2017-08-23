45cb6f873da5daf49aa66bbde4be5c5d468af519
Sergey Ivanov <sergey@brainjocks.com>
2017-08-23 15:44:46 +0300

    imitation of delivery

--- EasySendler.WebUI/Controllers/DeliveryController.cs
+++ EasySendler.WebUI/Controllers/DeliveryController.cs
@@ -1,17 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BuisenessLogicLayer;
+using EasySendler.Extensions;
+using EasySendler.Models.BusinessLogic;
+using EasySendler.Models.Controls;
+using Newtonsoft.Json;
 
 namespace EasySendler.Controllers
 {
     public class DeliveryController : Controller
     {
+        private readonly MySmtpEntities _db = new MySmtpEntities();
+
+        public DeliveryController()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                //cfg.CreateMap<RecipientList, RecipientListViewModel>();
+                //cfg.CreateMap<RecipientList, DropDownViewModel>()
+                //    .ForMember(dest => dest.Id, y => y.MapFrom(source => source.rlId))
+                //    .ForMember(dest => dest.Text, y => y.MapFrom(source => source.Name + "|" + source.Description));
+                //cfg.CreateMap<sp_getRecipientsByListIdResult, DualListViewModel>()
+                //    .ForMember(dest => dest.Id, y => y.MapFrom(source => source.RecipientId))
+                //    .ForMember(dest => dest.Text, y => y.MapFrom(source => source.Email))
+                //    .ForMember(dest => dest.IsSelected, y => y.MapFrom(source => source.Selected));
+                //cfg.CreateMap<Recipient, RecipientsViewModel>()
+                //    .ForMember(dest => dest.rId, y => y.MapFrom(source => source.RecipientId));
+            });
+        }
         // GET: Delivery
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [JsonExceptionFilter]
+        public JsonResult Run(string emaill, string name, string sureName, string templateId, string mailSettingsId)
+        {
+            int tmplId;
+            if (!int.TryParse(templateId, out tmplId))
+            {
+                throw new JsonException("Delivery.Run: templateId must be a number.");
+            }
+
+            int msId;
+            if (!int.TryParse(mailSettingsId, out msId))
+            {
+                throw new JsonException("Delivery.Run: mailSettingsId must be a number.");
+            }
+
+            //var recipients = _db.RecipientListsRelations.Where(x => x.rlId.Equals(rlId)).Select(x => x.Recipient).AsQueryable().ProjectTo<RecipientsViewModel>().ToList();
+
+            Thread.Sleep(1000);
+
+            var result = true;
+
+            return new JsonResult
+            {
+                Data = JsonConvert.SerializeObject(result),
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
         }
     }
 }
--- EasySendler.WebUI/Scripts/site/Delivery.js
+++ EasySendler.WebUI/Scripts/site/Delivery.js
@@ -5,12 +5,14 @@
     var $ddlRecipientLists = $("#ddlRecipientLists");
     var $ddlMailSettingsLists = $("#ddlMailSettingsLists");
     var $recipientAmount = $("#recipientAmount");
+    var $runProgress = $("#runProgress");
     var recipientsAmountUrl = $ddlRecipientLists.get(0).dataset.recipientsAmountUrl;
     var recipientsDetailsUrl = $ddlRecipientLists.get(0).dataset.recipientsDetailsUrl;
     var selectedReciientListId = null;
     var selectedTemplateId = null;
     var selectedMailSettingsId = null;
     var $btnRun = $("#btnRun");
+    var runUrl = $btnRun.get(0).dataset.runUrl;
     var $btnOpenRecipientList = $("#btnOpenRecipientList");
     var $modalOpenRecipientList = $("#recipientListDetails");
     var $modalTemplatesList = $("#templatesList");
@@ -47,7 +49,9 @@
     });
 
     $ddlRecipientLists.on("select2:unselect", function(e) {
-        $recipientAmount.val(0).trigger('change');
+        $recipientAmount.val(0).trigger("change");
+        $runProgress.val(0).trigger("change");
+        $runProgress.trigger("configure", { max: 100 });
         $btnOpenRecipientList.removeClass("active").addClass("disabled");
         selectedReciientListId = null;
         checkConditionsToRun();
@@ -63,7 +67,8 @@
             traditional: true,
             success: function (rawData) {
                 var data = JSON.parse(rawData);
-                $recipientAmount.val(data).trigger('change');
+                $recipientAmount.val(data).trigger("change");
+                $runProgress.trigger("configure", { max: data });
                 $btnOpenRecipientList.removeClass("disabled").addClass("active");
                 checkConditionsToRun();
             },
@@ -156,6 +161,125 @@
         selectedMailSettingsId = null;
         checkConditionsToRun();
     });
+
+    $btnRun.on("click", function (event) {
+        if ($(this).hasClass("disabled")) {
+            event.stopPropagation();
+        } else {
+            //$.ajax({
+            //    type: "GET",
+            //    url: $modalTemplatesList.get(0).dataset.templatesListUrl,
+            //    data: {},
+            //    dataType: "json",
+            //    traditional: true,
+            //    success: function (rawData) {
+            //        var data = JSON.parse(rawData);
+            //        //renderTemplate($modalBody, templatesListTemplate, { items: data });
+            //        //initTemplatesLinks();
+            //    },
+            //    error: getAjaxError
+            //});
+            $.ajax({
+                type: "GET",
+                url: recipientsDetailsUrl,
+                data: { id: selectedReciientListId },
+                dataType: "json",
+                traditional: true,
+                success: function (rawData) {
+                    var data = JSON.parse(rawData);
+
+                    sendEmail(data, 0);
+
+
+                    ////renderTemplate($modalBody, templateOpenRecipientList, { items: data });
+                    ////for (var j = 0; j < data.length; j++) {
+                    ////    var email = data[j].Email;
+                    ////    var name = data[j].Name;
+                    ////    var sureName = data[j].SureName;
+                    ////        var selectedtId = selectedTemplateId;
+                    ////        var selectedmsId = selectedMailSettingsId;
+                    ////        setTimeout(function(j) {
+                    ////            $runProgress.val(j + 1).trigger("change");
+                    ////        }, 1000);
+                    ////}
+                    //var promises = 0;
+                    //_.each(data, function(item, i, all) {
+                    //    var email = item.Email;
+                    //    var name = item.Name;
+                    //    var sureName = item.SureName;
+                    //    var selectedtId = selectedTemplateId;
+                    //    var selectedmsId = selectedMailSettingsId;
+                    //    //setTimeout(function() {
+                    //    //    $runProgress.val(i + 1).trigger("change");
+                    //    //}, 1000);
+
+                    //    //alert(i);
+
+
+                    //    var promise = $.ajax({
+                    //        type: "GET",
+                    //        dataType: "json",
+                    //        data: { email: email, name: name, sureName: sureName, templateId: selectedtId, mailSettingsId: selectedmsId },
+                    //        url: runUrl,
+                    //        success: function (response) {
+                    //            alert("success" + i);
+                    //            //var data = {
+                    //            //    temperature: response.currently.temperature,
+                    //            //    dewPoint: response.currently.dewPoint,
+                    //            //    humidity: response.currently.humidity,
+                    //            //    windSpeed: response.currently.windSpeed,
+                    //            //    windBearing: response.currently.windBearing,
+                    //            //    pressure: response.currently.pressure
+                    //            //};
+
+                    //            //historyData[moment().year() - i] = data
+
+                    //            //console.log(data);
+                    //        }
+                    //    });
+
+                    //    promise.done(function () {
+                    //        alert("done" + i);
+                    //        promises++;
+                    //        $runProgress.val(promises).trigger("change");
+                    //        console.log(promises);
+                    //        if (promises === all.length) {
+                    //            //bindweatherHistoryTable();
+                    //            alert('finish');
+                    //        }
+                    //    });
+
+
+                    //});
+                },
+                error: getAjaxError
+            });
+        }
+    });
+
+   // function sendEmail(email, name, sureName, selectedtId, selectedmsId) {
+    function sendEmail(data, index) {
+        var item = data[index];
+        var email = item.Email;
+        var name = item.Name;
+        var sureName = item.SureName;
+        var selectedtId = selectedTemplateId;
+        var selectedmsId = selectedMailSettingsId;
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            data: { email: email, name: name, sureName: sureName, templateId: selectedtId, mailSettingsId: selectedmsId },
+            url: runUrl,
+            success: function (response) {
+                $runProgress.val(parseInt($runProgress.val(), 10) + 1).trigger("change");
+               // alert("success");
+                if (index < data.length - 1) {
+                    sendEmail(data, index + 1);
+                }
+            },
+            error: getAjaxError
+        });
+    }
 
     function checkConditionsToRun() {
         if (selectedReciientListId && selectedTemplateId && selectedMailSettingsId) {
