d3a051729e857e965f939abb7639466434db0539
Sergey Ivanov <ivengo2000@rambler.ru>
2018-07-15 12:23:46 +0300

    added Description for List dto in web API

--- EasySendler.WebUI/Controllers/WebApiV2Controller.cs
+++ EasySendler.WebUI/Controllers/WebApiV2Controller.cs
@@ -1,11 +1,17 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
 using System.Text;
 using System.Web.Http;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using BuisenessLogicLayer;
+using BuisenessLogicLayer.Abstractions;
+using BuisenessLogicLayer.Models;
+using BuisenessLogicLayer.Services;
+using EasySendler.Models.BusinessLogic;
 using EasySendler.Models.Controls;
 using Newtonsoft.Json;
 
@@ -15,20 +21,29 @@ namespace EasySendler.Controllers
     public class WebApiV2Controller : ApiController
     {
         private readonly MySmtpEntities _db = new MySmtpEntities();
+        private readonly IMailService _mailService;
+        private int descriptionTruncateValue = 150;
 
         public WebApiV2Controller()
         {
+            _mailService = new MailService();
+
             Mapper.Initialize(cfg =>
             {
-                cfg.CreateMap<RecipientList, DropDownViewModel>()
+                cfg.CreateMap<MailSetting, MailSettingViewModel>();
+                cfg.CreateMap<Template, TemplateViewModel>();
+                cfg.CreateMap<RecipientList, MobileListViewModel>()
                     .ForMember(dest => dest.Id, y => y.MapFrom(source => source.rlId))
-                    .ForMember(dest => dest.Text, y => y.MapFrom(source => source.Name + " | " + source.Description));
-                cfg.CreateMap<MailSetting, DropDownViewModel>()
+                    .ForMember(dest => dest.Text, y => y.MapFrom(source => source.Name))
+                    .ForMember(dest => dest.Description, y => y.MapFrom(source => source.Description.Length > descriptionTruncateValue ? source.Description.Substring(0, descriptionTruncateValue) + "..." : source.Description));
+                cfg.CreateMap<MailSetting, MobileListViewModel>()
                     .ForMember(dest => dest.Id, y => y.MapFrom(source => source.MailSettingsId))
-                    .ForMember(dest => dest.Text, y => y.MapFrom(source => source.Email + " | " + source.Description));
-                cfg.CreateMap<Template, DropDownViewModel>()
+                    .ForMember(dest => dest.Text, y => y.MapFrom(source => source.Email))
+                    .ForMember(dest => dest.Description, y => y.MapFrom(source => source.Description.Length > descriptionTruncateValue ? source.Description.Substring(0, descriptionTruncateValue) + "..." : source.Description));
+                cfg.CreateMap<Template, MobileListViewModel>()
                     .ForMember(dest => dest.Id, y => y.MapFrom(source => source.TemplateId))
-                    .ForMember(dest => dest.Text, y => y.MapFrom(source => source.Name + " | " + source.Description));
+                    .ForMember(dest => dest.Text, y => y.MapFrom(source => source.Name))
+                    .ForMember(dest => dest.Description, y => y.MapFrom(source => source.Description.Length > descriptionTruncateValue ? source.Description.Substring(0, descriptionTruncateValue) + "..." : source.Description));
             });
         }
 
@@ -49,7 +64,7 @@ namespace EasySendler.Controllers
                 return CreateHttpResponseMessage("The 'start' parameter must be an integer", HttpStatusCode.BadRequest);
             }
 
-            var results = _db.RecipientLists.OrderBy(x => x.rlId).Skip((intStart - 1) * intLimit).Take(intLimit).ProjectTo<DropDownViewModel>().ToList();
+            var results = _db.RecipientLists.OrderBy(x => x.rlId).Skip((intStart - 1) * intLimit).Take(intLimit).ProjectTo<MobileListViewModel>().ToList();
 
             return CreateHttpResponseMessage(JsonConvert.SerializeObject(results));
         }
@@ -71,7 +86,7 @@ namespace EasySendler.Controllers
                 return CreateHttpResponseMessage("The 'start' parameter must be an integer", HttpStatusCode.BadRequest);
             }
 
-            var results = _db.MailSettings.OrderBy(x => x.MailSettingsId).Skip((intStart - 1) * intLimit).Take(intLimit).ProjectTo<DropDownViewModel>().ToList();
+            var results = _db.MailSettings.OrderBy(x => x.MailSettingsId).Skip((intStart - 1) * intLimit).Take(intLimit).ProjectTo<MobileListViewModel>().ToList();
 
             return CreateHttpResponseMessage(JsonConvert.SerializeObject(results));
         }
@@ -93,9 +108,80 @@ namespace EasySendler.Controllers
                 return CreateHttpResponseMessage("The 'start' parameter must be an integer", HttpStatusCode.BadRequest);
             }
 
-            var results = _db.Templates.OrderBy(x => x.TemplateId).Skip((intStart - 1) * intLimit).Take(intLimit).ProjectTo<DropDownViewModel>().ToList();
+            var results = _db.Templates.OrderBy(x => x.TemplateId).Skip((intStart - 1) * intLimit).Take(intLimit).ProjectTo<MobileListViewModel>().ToList();
 
             return CreateHttpResponseMessage(JsonConvert.SerializeObject(results));
+        }
+
+        // GET: api/run/get?rid=3&tid=1&sid=1
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("run/get")]
+        public HttpResponseMessage Run(string rid, string tid, string sid)
+        {
+            int rlId;
+            if (!int.TryParse(rid, out rlId))
+            {
+                throw new JsonException("Api.Run: rid must be a number.");
+            }
+
+            int tmplId;
+            if (!int.TryParse(tid, out tmplId))
+            {
+                throw new JsonException("Api.Run: tid must be a number.");
+            }
+
+            int msId;
+            if (!int.TryParse(sid, out msId))
+            {
+                throw new JsonException("Api.Run: sid must be a number.");
+            }
+
+            var mailSetting = Mapper.Map<MailSettingViewModel>(_db.MailSettings.Find(msId));
+
+            int port;
+            if (!int.TryParse(mailSetting.SMTPPort, out port))
+            {
+                throw new JsonException("Api.Run: port must be a number.");
+            }
+
+            var mailServerInfo = new MailServerInfo
+            {
+                EnableSsl = mailSetting.EnableSsl,
+                Name = mailSetting.SMTP,
+                Password = mailSetting.Password,
+                Port = port,
+                UserName = mailSetting.Email
+            };
+
+            _mailService.SetSmtpSettings(mailServerInfo);
+
+            var template = Mapper.Map<TemplateViewModel>(_db.Templates.Find(tmplId));
+
+            var recipientsList = _db.RecipientLists.Where(x => x.rlId == rlId).SelectMany(y => y.RecipientListsRelations.Select(r=>r.Recipient)).ToList();
+
+            var result = new List<ErrorInfo>();
+
+            foreach (var recipient in recipientsList)
+            {
+                var generatedBody = template.Body.Replace("{{name}}", recipient.Name + " " + recipient.SureName).Replace("{{email}}", recipient.Email);
+
+                var emailMessage = new MailMessage(mailSetting.Email, recipient.Email, template.Name, generatedBody) //ToDo: add new filed into Template entity named Subject.
+                {
+                    IsBodyHtml = true
+                };
+
+                var resultMessage = _mailService.SendMail(emailMessage);
+
+                var resultLine = new ErrorInfo
+                {
+                    ResultMessage = resultMessage,
+                    IsSuccessful = resultMessage.Equals("Message has been sent")
+                };
+
+                result.Add(resultLine);
+            }
+
+            return CreateHttpResponseMessage(JsonConvert.SerializeObject(result));
         }
 
         private HttpResponseMessage CreateHttpResponseMessage(string results, HttpStatusCode status = HttpStatusCode.OK)
---
+++ EasySendler.WebUI/Models/Controls/MobileListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace EasySendler.Models.Controls
+{
+    public class MobileListViewModel: DropDownViewModel
+    {
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+    }
+}
